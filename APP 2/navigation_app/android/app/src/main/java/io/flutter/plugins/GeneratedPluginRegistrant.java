248d0064f1824c1b31edf8cb9d2abaf726902528
sofiagarciad <sgarcia74@gatech.edu>
2025-04-09 20:08:13 -0400

    main.dart updated

--- APP 2/navigation_app/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ APP 2/navigation_app/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -15,5 +15,10 @@ import io.flutter.embedding.engine.FlutterEngine;
 public final class GeneratedPluginRegistrant {
   private static final String TAG = "GeneratedPluginRegistrant";
   public static void registerWith(@NonNull FlutterEngine flutterEngine) {
+    try {
+      flutterEngine.getPlugins().add(new com.signify.hue.flutterreactiveble.ReactiveBlePlugin());
+    } catch (Exception e) {
+      Log.e(TAG, "Error registering plugin reactive_ble_mobile, com.signify.hue.flutterreactiveble.ReactiveBlePlugin", e);
+    }
   }
 }
--- APP 2/navigation_app/ios/Runner/GeneratedPluginRegistrant.m
+++ APP 2/navigation_app/ios/Runner/GeneratedPluginRegistrant.m
@@ -6,9 +6,16 @@
 
 #import "GeneratedPluginRegistrant.h"
 
+#if __has_include(<reactive_ble_mobile/ReactiveBlePlugin.h>)
+#import <reactive_ble_mobile/ReactiveBlePlugin.h>
+#else
+@import reactive_ble_mobile;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [ReactiveBlePlugin registerWithRegistrar:[registry registrarForPlugin:@"ReactiveBlePlugin"]];
 }
 
 @end
